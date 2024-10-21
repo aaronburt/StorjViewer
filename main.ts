826a7ae94243c24a721fa6d11a0ca0d767b5a58d
aaronburt <email@aaronburt.co.uk>
2024-10-21 03:34:03 +0100

    feat: :zap: Refactor and adding /live feature caching using filesys

--- main.ts
+++ main.ts
@@ -1,13 +1,45 @@
+// deno-lint-ignore-file no-unused-vars no-explicit-any
 import getImagesFromR2 from "./aws.ts";
+
+
+async function buildCacheFile(){
+  const content = await getImagesFromR2()
+  return await Deno.writeTextFile(".cache", JSON.stringify({ content: content, expires: (Date.now() * 1000) + 60 * 1000 })) 
+}
+
+async function readCacheFile(){
+  const cacheFile = await Deno.readFile(".cache");
+  const cacheJson = new TextDecoder().decode(cacheFile.buffer);
+  const parsedJson = JSON.parse(cacheJson);
+  return parsedJson;
+}
 
 Deno.serve({ port: 80 }, async(request: Request) => {
   const pathname = new URL(request.url).pathname;
+  if (pathname.startsWith('/live')) {
+    try {
+      const parsedJson = await readCacheFile()
+      if(parsedJson.expires){
+        if(parseInt(parsedJson.expires) < Date.now() * 1000){
+          return Response.json(parsedJson);
+        }
+      }
 
-  if(pathname.startsWith('/live')){
-    const images: Record<string, string> = await getImagesFromR2();
-    return Response.json(images)
+      await buildCacheFile();      
+      return Response.json(await readCacheFile())
+
+    } catch(error: any){
+      await buildCacheFile();
+    }
+
+    return Response.json(await readCacheFile())
+  }
+  
+  if(pathname.startsWith('/static/css')){
+      const stylesheet = await Deno.open("./static/stylesheet.css", { read: true });
+      return new Response(stylesheet.readable)
   }
 
-  const file = await Deno.open("index.html", { read: true });
-  return new Response(file.readable);
+  const file = await Deno.open("./static/index.html", { read: true });
+  return new Response(file.readable, { headers: { "content-type": "text/html" } });
 })
